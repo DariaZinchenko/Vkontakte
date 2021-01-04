77514ffc4bcf6e641092fb2ad6c09c515308d03f
DariaZinchenko <dariadaria0593@gmail.com>
2021-01-04 22:17:41 +0300

    Update Post.java and CommentInfo.java

--- src/main/java/domain/CommentInfo.java
+++ src/main/java/domain/CommentInfo.java
@@ -1,4 +1,40 @@
 package domain;
 
 public class CommentInfo {
+    private int id;
+    private int userId;
+    private String text;
+    private int publicationTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getPublicationTime() {
+        return publicationTime;
+    }
+
+    public void setPublicationTime(int publicationTime) {
+        this.publicationTime = publicationTime;
+    }
 }
--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -17,4 +17,123 @@ public class Post {
     private String source;
     private int copyrightUserId;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getAmountOfViews() {
+        return amountOfViews;
+    }
+
+    public void setAmountOfViews(int amountOfViews) {
+        this.amountOfViews = amountOfViews;
+    }
+
+    public int getPublicationTime() {
+        return publicationTime;
+    }
+
+    public void setPublicationTime(int publicationTime) {
+        this.publicationTime = publicationTime;
+    }
+
+    public Privacy getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(Privacy privacy) {
+        this.privacy = privacy;
+    }
+
+    public int[] getWhoCanSeeId() {
+        return whoCanSeeId;
+    }
+
+    public void setWhoCanSeeId(int[] whoCanSeeId) {
+        this.whoCanSeeId = whoCanSeeId;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public int[] getWhoRepostId() {
+        return whoRepostId;
+    }
+
+    public void setWhoRepostId(int[] whoRepostId) {
+        this.whoRepostId = whoRepostId;
+    }
+
+    public int[] getWhoLikedId() {
+        return whoLikedId;
+    }
+
+    public void setWhoLikedId(int[] whoLikedId) {
+        this.whoLikedId = whoLikedId;
+    }
+
+    public CommentInfo[] getCommentsInfoId() {
+        return commentsInfoId;
+    }
+
+    public void setCommentsInfoId(CommentInfo[] commentsInfoId) {
+        this.commentsInfoId = commentsInfoId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCommentable() {
+        return isCommentable;
+    }
+
+    public void setCommentable(boolean commentable) {
+        isCommentable = commentable;
+    }
+
+    public boolean isSendNotification() {
+        return isSendNotification;
+    }
+
+    public void setSendNotification(boolean sendNotification) {
+        isSendNotification = sendNotification;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public int getCopyrightUserId() {
+        return copyrightUserId;
+    }
+
+    public void setCopyrightUserId(int copyrightUserId) {
+        this.copyrightUserId = copyrightUserId;
+    }
 }
