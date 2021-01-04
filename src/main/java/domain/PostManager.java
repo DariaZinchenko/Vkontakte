b26e47d6ba193bc0a4a507644f483dfad7a719e3
DariaZinchenko <dariadaria0593@gmail.com>
2021-01-05 00:01:25 +0300

    Add PostManager.java (task 2)

---
+++ src/main/java/domain/PostManager.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPosts() {
+        return null;
+    }
+
+    public Post[] Search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void Delete(int ownerId, int postId) {
+    }
+}
